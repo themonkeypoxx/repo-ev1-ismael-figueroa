42738983538596f57c4b049d6d3cfd4ef882fb15
damonkepoxx <isma.figueroa06@gmail.com>
2026-09-16 17:57:08 -0300

    Correccion creacion de proyecto sumativa

--- sumativa/settings.py
+++ sumativa/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1',
 ]
 
 MIDDLEWARE = [
@@ -52,18 +51,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'sumativa.urls'
 
-import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static',
             ],
         },
     },
